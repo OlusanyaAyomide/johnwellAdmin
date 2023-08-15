387e36478a678b52ba525291a7a765efacac731c
AYOMIDE <AYOMIDE>
2023-08-15 16:43:59 +0100

    First-prod-commit

--- src/components/dashboard/TopCards.tsx
+++ src/components/dashboard/TopCards.tsx
@@ -3,16 +3,17 @@ import { Card } from '../ui/card'
 
 
 interface ITopCards{
-    count:string | number
+    count:number
     text:string
 
 
 }
 export default function TopCards({count,text}:ITopCards) {
+  const hasloaded = count >=0
   return (
     <Card className='w-[240px] min-h-[210px] shrink-0 py-16 flex place-content-center cursor-pointer'>
     <div>
-      {count ?<>
+      {hasloaded ?<>
         <h1 className="font-extrabold text-center text-5xl text-main">{count}</h1>
         <h1 className="text-center text-base mt-[2px]">{text}</h1>
       </>
